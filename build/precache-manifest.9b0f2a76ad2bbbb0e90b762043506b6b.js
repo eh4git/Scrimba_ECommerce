a248b0dc27537b34df7a19054da262f7df9cee0e
eh4git <ehirsch760@gmail.com>
2022-09-20 16:28:42 -0700

    build

---
+++ build/precache-manifest.9b0f2a76ad2bbbb0e90b762043506b6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1831d56dc97715a52b07cb09179b7f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e89dc813ace8755cf9e",
+    "url": "/static/js/2.6608b316.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.6608b316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb836b5bb441cdc68ed0",
+    "url": "/static/js/main.b8a39c01.chunk.js"
+  },
+  {
+    "revision": "7e6a8db47024c1c6b2b3",
+    "url": "/static/js/runtime-main.7e0e0399.js"
+  }
+]);
